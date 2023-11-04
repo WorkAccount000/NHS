eaf52209c3b0d15a4234da1e9c793557abba4ff2
T.S <s.work.mail.000@gmail.com>
2023-11-04 10:13:52 +0900

    Revert "edit generatorConfig"
    This reverts commit 510abf8e03573c0db6067774f7ab6196a1b7c39b.

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.UserSearchRequest;
-import com.example.demo.entity.User;
+import com.example.demo.model.User;
 import com.example.demo.repository.UserMapper;
 
 @Service
