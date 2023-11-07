ad0b158c016e395287cfa5c181f6b6108a4ddb9a
T.S <s.work.mail.000@gmail.com>
2023-11-07 09:47:19 +0900

    Delete dto

--- src/main/java/com/example/demo/dto/UserSearchRequest.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dto;
-
-import java.io.Serializable;
-
-public class UserSearchRequest implements Serializable {
-	private String userId;
-}
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,9 +2,8 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
-import com.example.demo.dto.UserSearchRequest;
 import com.example.demo.mapper.UserMapper;
-import com.example.demo.model.User;
+//import com.example.demo.model.User;
 
 @Service
 public class UserService {
