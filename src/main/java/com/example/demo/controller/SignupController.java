fd5c590124c69dfba440c1bcd4f61e41547a9bcf
T.S <s.work.mail.000@gmail.com>
2023-11-09 21:16:53 +0900

    Add validation function.

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -1,16 +1,15 @@
 package com.example.demo.controller;
 
-import java.time.LocalDate;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.form.SignupForm;
-import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 
 @Controller
@@ -23,27 +22,38 @@ public class SignupController {
 	}
 	
 	@GetMapping("/signup")
-	public String signup(Model model) {
-		model.addAttribute("signupForm", new SignupForm());
+	public String getSignup(Model model, @ModelAttribute SignupForm form) {
+
+		System.out.println(form);
+		model.addAttribute("form", form);
 		return "signup/signup";
 	}
 	
-	@PostMapping("/register")
-	public String register(@ModelAttribute SignupForm form, Model model) {
-		User user = new User();
-		boolean registrable = userService.checkRegistrable(form.getUserId());
-		String result = "";
-		if(registrable) {
-			user.importForm(form);
-			// post で受け取ったUserクラスの password をハッシュ化
-			user.setPassword(userService.getHashString(user.getPassword()));
-			user.setCreatedDate(LocalDate.now());
-			userService.register(user);
-			result = "top/top";
-		} else {
-			model.addAttribute(form);
-			result = "signup/signup";
+	@PostMapping("/signup")
+	public String register( Model model,
+			//@Validated(GroupOrder.class) @ModelAttribute SignupForm form,
+			@Validated @ModelAttribute SignupForm form,
+			BindingResult result) {
+		if(result.hasErrors()) {
+			System.out.println("Validated error.");
+			System.out.println(form);
+			return getSignup(model, form);
 		}
-		return result;
+//		User user = new User();
+//		boolean registrable = userService.checkRegistrable(form.getUserId());
+//		String result = "";
+//		if(registrable) {
+//			user.importForm(form);
+//			// post で受け取ったUserクラスの password をハッシュ化
+//			user.setPassword(userService.getHashString(user.getPassword()));
+//			user.setCreatedDate(LocalDate.now());
+//			userService.register(user);
+//			result = "top/top";
+//		} else {
+//			model.addAttribute(form);
+//			result = "signup/signup";
+//		}
+//		return result;
+		return "top/top";
 	}
 }
--- src/main/java/com/example/demo/form/SignupForm.java
+++ src/main/java/com/example/demo/form/SignupForm.java
@@ -1,29 +1,38 @@
 package com.example.demo.form;
 
-import org.hibernate.validator.constraints.Length;
+//import org.hibernate.validator.constraints.Length;
 
-import jakarta.validation.constraints.Email;
+//import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+//import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class SignupForm {
-	@NotBlank(groups = ValidGroup1.class)
-	@Pattern(regexp="^[!-~]+$", message="{0}は半角英数字のみで入力してください。", groups = ValidGroup3.class)
-	@Length(min=3, max=10, groups = ValidGroup4.class)
-	@Pattern(regexp="^[0-9A-Za-z]+$", message="{0}は特殊記号を使用しないでください。", groups = ValidGroup5.class)
+//	@NotBlank(groups = ValidGroup1.class)
+//	@Pattern(regexp="^[!-~]+$", message="{0}は半角英数字のみで入力してください。", groups = ValidGroup3.class)
+//	@Length(min=3, max=10, groups = ValidGroup4.class)
+//	@Pattern(regexp="^[0-9A-Za-z]+$", message="{0}は特殊記号を使用しないでください。", groups = ValidGroup5.class)
+//	private String userId;
+	
+	@NotBlank
 	private String userId;
 	
-	@NotBlank(groups = ValidGroup1.class)
-	@Email(groups = ValidGroup2.class)
-	@Pattern(regexp="^[!-~]+$", message="{0}は半角英数字のみで入力してください。", groups = ValidGroup3.class)
+//	@NotBlank(groups = ValidGroup1.class)
+//	@Email(groups = ValidGroup2.class)
+//	@Pattern(regexp="^[!-~]+$", message="{0}は半角英数字のみで入力してください。", groups = ValidGroup3.class)
+//	private String email;
+	
+	@NotBlank
 	private String email;
 	
-	@NotBlank(groups = ValidGroup1.class)
-	@Pattern(regexp="^[!-~]+$", message="{0}は半角英数字のみで入力してください。", groups = ValidGroup3.class)
-	@Length(min=3, max=10, groups = ValidGroup4.class)
-	@Pattern(regexp="^[!-~]+$", message="{0}は大文字小文字数字を入力してください。", groups = ValidGroup5.class)
-	@Pattern(regexp="^[0-9A-Za-z]+$", message="{0}は特殊記号を使用しないでください。", groups = ValidGroup6.class)
+//	@NotBlank(groups = ValidGroup1.class)
+//	@Pattern(regexp="^[!-~]+$", message="{0}は半角英数字のみで入力してください。", groups = ValidGroup3.class)
+//	@Length(min=3, max=10, groups = ValidGroup4.class)
+//	@Pattern(regexp="^[!-~]+$", message="{0}は大文字小文字数字を入力してください。", groups = ValidGroup5.class)
+//	@Pattern(regexp="^[0-9A-Za-z]+$", message="{0}は特殊記号を使用しないでください。", groups = ValidGroup6.class)
+//	private String password;
+	
+	@NotBlank
 	private String password;
 }
