da9e60bd4515f6c3708e8eed67ccf2c72364a480
seali <seali@DESKTOP-H6PB507>
2023-11-17 23:42:24 +0900

    11/17
    ・以下のファイルをtemplets直下に移動（パス簡略化のため)
    input.html
    inputCheck.html

--- src/main/java/com/example/demo/controller/InputController.java
+++ src/main/java/com/example/demo/controller/InputController.java
@@ -1,23 +1,56 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import java.util.List;
 
-import com.example.demo.form.InputRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class InputController {
-	@RequestMapping("/input")
-	public String nyuuryoku(){
-		return "/input/input"; 
-	}
+    @RequestMapping("/input")
+    public String input() {
+        return "input";
+    }
 
-	@RequestMapping("/inputCheck")
-	public ModelAndView inputResult(@ModelAttribute InputRequest ir, ModelAndView mav) {
-		mav.addObject("ir", ir);
-		mav.setViewName("/input/inputCheck.html"); 
-		return mav;
-	}
+    @RequestMapping("/inputCheck")
+    public ModelAndView inputResult(
+            @RequestParam("name") String name,
+            @RequestParam("subName") String subName,
+            @RequestParam("gender") String gender,
+            @RequestParam(value = "hobby", required = false) List<String> hobby,
+            @RequestParam("comment") String comment,
+            ModelAndView mav) {
+
+        boolean hasError = false;
+
+        // バリデーションチェック
+        if (name == null || name.isEmpty()) {
+            mav.addObject("nameError", "名前は必須です");
+            hasError = true;
+        }
+        // 他のフィールドに対するバリデーションも同様に行う
+
+        if (hasError) {
+            // エラーがある場合、入力フォームに戻る
+            mav.setViewName("input");
+            // 入力されたデータを再設定
+            mav.addObject("name", name);
+            mav.addObject("subName", subName);
+            mav.addObject("gender", gender);
+            mav.addObject("hobby", hobby);
+            mav.addObject("comment", comment);
+            return mav;
+        }
+
+        // エラーがない場合、データをビューに追加して結果画面に遷移
+        mav.addObject("name", name);
+        mav.addObject("subName", subName);
+        mav.addObject("gender", gender);
+        mav.addObject("hobby", hobby);
+        mav.addObject("comment", comment);
+        mav.setViewName("inputCheck");
+        return mav;
+    }
 }
