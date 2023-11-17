27782d70fb4738f0ee3dd5ee942e81a70e28aaa6
seali <seali@DESKTOP-H6PB507>
2023-11-18 02:27:50 +0900

    11/18 2:26
    ＜残りタスク＞
    ・入力画面:未入力エラー、エラーチェック
    ・確認画面:完了ボタンを押したときのDBへの登録

--- src/main/java/com/example/demo/controller/InputController.java
+++ src/main/java/com/example/demo/controller/InputController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class InputController {
-    @RequestMapping("/input")
+    @GetMapping("/input")
     public String input() {
         return "input";
     }
