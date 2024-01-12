0bf017a252b1b1d9e56fc486c4afdadc3d939754
nishimura-a <nishimura.atsuki1@gmail.com>
2024-01-13 02:32:19 +0900

    不要ファイル削除及び微調整

---
+++ src/main/java/com/example/demo/request/IntroductionsRequest.java
@@ -0,0 +1,16 @@
+package com.example.demo.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class IntroductionsRequest {
+	private String id;
+	private String name;
+	private String kana;
+	private String gender;
+	private String hobby;
+	private String word;
+}
