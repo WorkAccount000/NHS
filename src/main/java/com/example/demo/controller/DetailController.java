5897d8af8a7d115d464b50ecadb7506f47b4b130
nishimura-a <nishimura.atsuki1@gmail.com>
2024-01-13 02:35:35 +0900

    データ削除のポップアップ作成

--- src/main/java/com/example/demo/controller/DetailController.java
+++ src/main/java/com/example/demo/controller/DetailController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.form.IntroductionsRequest;
+import com.example.demo.request.IntroductionsRequest;
 
 @Controller
 
---
+++ src/main/resources/static/js/delete.js
@@ -0,0 +1,6 @@
+//削除確認ポップアップ
+let deleteBtn = document.getElementById('delete');
+function Popup(id){
+	confirm('ID:' + id + 'のデータを削除してもよろしいですか？');
+};
+
